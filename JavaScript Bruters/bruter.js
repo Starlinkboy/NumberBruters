c597419aa0425c2f34c8261a89e2e2d2502f6cd8
Starlinkboy <bhallavibhav@gmail.com>
2022-11-11 23:17:00 +0400

    bgBlue --> blueBright

--- JavaScript Bruters/bruter.js
+++ JavaScript Bruters/bruter.js
@@ -1,12 +1,12 @@
 const prompt = require('prompt-sync')({ sigint: true });
-const { greenBright, cyanBright, magentaBright, red, bgBlue } = require("colorette");
+const { greenBright, cyanBright, magentaBright, red, blueBright } = require("colorette");
 const Discord = require("discord.js");
 const { WebhookClient } = require('discord.js');
 const a = 6969
 const b = 10000
 let attempts = 0
 
-const webhook = prompt(bgBlue("Enter Webhook Url: "))
+const webhook = prompt(blueBright("Enter Webhook Url: "))
 console.log(magentaBright("Starting Bruter..."))
 const myHook = new WebhookClient({ url: webhook });
 
